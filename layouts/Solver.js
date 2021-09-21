ebb1b548eb98a933e0814ed660ff6ab067bdc4e6
dineshyadav3169 <dineshyadav3169@gmail.com>
2021-09-21 21:59:33 +0530

    small fixes

--- layouts/Solver.js
+++ layouts/Solver.js
@@ -1,23 +1,31 @@
 import Container from '@/components/container/Container';
 import PrevNext from '@/components/PrevNext';
+import Solution from './Solution';
 
-export default function Solver({ pageTitle, pageDescription, heading, slideData, currentPage, children}) {
-  
-  let linkArray = []
-  slideData.map(
-    t=>{
-      t.slideNavData.map((u)=>{
-        linkArray.push(u.link)
-      })
-    }
-  )
+export default function Solver({
+  pageTitle,
+  pageDescription,
+  heading,
+  slideData,
+  currentPage,
+  solution,
+  question,
+  children
+}) {
+  let linkArray = [];
+  slideData.map((t) => {
+    t.slideNavData.map((u) => {
+      linkArray.push(u.link);
+    });
+  });
 
-  const i=linkArray.indexOf(currentPage);
-  const previousLink=linkArray[i==0?linkArray.length:i-1] || false;
-  const nextLink=linkArray[i==linkArray.length-1?linkArray.length:i+1] || false;
+  const i = linkArray.indexOf(currentPage);
+  const previousLink = linkArray[i == 0 ? linkArray.length : i - 1] || false;
+  const nextLink =
+    linkArray[i == linkArray.length - 1 ? linkArray.length : i + 1] || false;
 
   return (
-    <Container 
+    <Container
       title={pageTitle}
       description={pageDescription}
       hasSlideContent={true}
@@ -28,6 +36,43 @@ export default function Solver({ pageTitle, pageDescription, heading, slideData,
       </h1>
       <PrevNext prevoiusLink={previousLink} nextLink={nextLink} />
       {children}
+
+      <div role="tablist" className="mt-10 text-left">
+        <ul className="list-none">
+          <li className="inline-block">
+            <div
+              id="t1"
+              tabIndex="0"
+              role="tab"
+              aria-selected="true"
+              aria-controls="t1-panel"
+              className="cursor-pointer"
+            >
+              <div className="border-b-2 border-blue-500 relative py-3 px-5">
+                  <span className="">Solution</span>
+              </div>
+            </div>
+          </li>
+          <li className="inline-block">
+            <div
+              id="t2"
+              tabIndex="-1"
+              role="tab"
+              aria-selected="false"
+              aria-controls="t2-panel"
+              className="cursor-pointer"
+            >
+              <div className="relative py-3 px-5">
+                  <span className="FancyTabs-tabText">Examples</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <hr className="border-2 border-gray-200  w-full" />
+      </div>
+      
+      {solution && <Solution question={question}>{solution}</Solution>}
+
     </Container>
   );
 }
