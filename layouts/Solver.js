d5d94d39c9cb3bf91d945dc20b9f813c4ba1dec8
dineshyadav3169 <dineshyadav3169@gmail.com>
2021-09-22 16:42:17 +0530

    tab system made

--- layouts/Solver.js
+++ layouts/Solver.js
@@ -1,5 +1,6 @@
 import Container from '@/components/container/Container';
 import PrevNext from '@/components/PrevNext';
+import { useEffect, useRef, useState } from 'react';
 import Solution from './Solution';
 
 export default function Solver({
@@ -12,6 +13,9 @@ export default function Solver({
   question,
   children
 }) {
+  const [currentTab, setcurrentTab] = useState('example');
+  const dummyScrollView = useRef();
+
   let linkArray = [];
   slideData.map((t) => {
     t.slideNavData.map((u) => {
@@ -23,6 +27,13 @@ export default function Solver({
   const previousLink = linkArray[i == 0 ? linkArray.length : i - 1] || false;
   const nextLink =
     linkArray[i == linkArray.length - 1 ? linkArray.length : i + 1] || false;
+
+  useEffect(() => {
+    if (solution !== false) {
+      setcurrentTab('solution');
+      dummyScrollView.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [solution]);
 
   return (
     <Container
@@ -37,42 +48,80 @@ export default function Solver({
       <PrevNext prevoiusLink={previousLink} nextLink={nextLink} />
       {children}
 
-      <div role="tablist" className="mt-10 text-left">
-        <ul className="list-none">
-          <li className="inline-block">
+      <div role="tablist" className={`mt-10 text-left sm:px-12`}>
+        <ul ref={dummyScrollView} className="list-none w-full">
+          <li className="inline-block w-6/12">
             <div
               id="t1"
               tabIndex="0"
               role="tab"
-              aria-selected="true"
+              aria-selected="false"
               aria-controls="t1-panel"
-              className="cursor-pointer"
+              className="cursor-pointer text-center"
+              onClick={() => {
+                setcurrentTab('solution');
+              }}
             >
-              <div className="border-b-2 border-blue-500 relative py-3 px-5">
-                  <span className="">Solution</span>
+              <div
+                className={`border-b-2 ${
+                  currentTab === 'solution'
+                    ? 'border-blue-500 text-blue-600'
+                    : 'border-gray-200'
+                } relative py-3 px-5 transition duration-300 ease-in-out hover:bg-blue-50`}
+              >
+                <span className="">Solution</span>
               </div>
             </div>
           </li>
-          <li className="inline-block">
+          <li className="inline-block w-6/12">
             <div
               id="t2"
               tabIndex="-1"
               role="tab"
-              aria-selected="false"
+              aria-selected="true"
               aria-controls="t2-panel"
-              className="cursor-pointer"
+              className="cursor-pointer text-center"
+              onClick={() => {
+                setcurrentTab('example');
+              }}
             >
-              <div className="relative py-3 px-5">
-                  <span className="FancyTabs-tabText">Examples</span>
+              <div
+                className={`border-b-2 ${
+                  currentTab === 'example'
+                    ? 'border-blue-500 text-blue-600'
+                    : 'border-gray-200'
+                } relative py-3 px-5 transition duration-300 ease-in-out hover:bg-blue-50`}
+              >
+                <span className="">Examples</span>
               </div>
             </div>
           </li>
         </ul>
-        <hr className="border-2 border-gray-200  w-full" />
       </div>
-      
-      {solution && <Solution question={question}>{solution}</Solution>}
 
+      {currentTab === 'example' ? (
+        <div
+          id="t2-panel"
+          role="tabpanel"
+          aria-hidden="false"
+          aria-describedby="t2"
+        >
+          These are examples
+        </div>
+      ) : (
+        <div
+          id="t1-panel"
+          role="tabpanel"
+          aria-hidden="true"
+          aria-describedby="t1"
+        >
+          {solution ? (
+            <Solution question={question}>{solution}</Solution>
+          ) : (
+            <p>Your solution will appear here.</p>
+          )}
+        </div>
+      )}
     </Container>
   );
 }
