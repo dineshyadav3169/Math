9b9806d4db292a6c593d3c7b0b7bbc0b2adf89e1
Dinesh Kumar <dineshyadav3169@gmail.com>
2022-06-29 21:13:14 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,15 +7,14 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://www.google-analytics.com"/>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-HCY0ZSGCPS" strategy="afterInteractive"></Script>
           <Script id="google-analytics" strategy="afterInteractive">
             {`
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-              ga('create', 'UA-190740309-4', 'auto');
-              ga('send', 'pageview');
+              gtag('config', 'G-HCY0ZSGCPS');
             `}
           </Script>
           {/* <link
